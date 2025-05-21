cf20b79d7a4d6374d4a93afcf2098acd1d6e3ea9
splguyjr <splguyjr@naver.com>
2025-05-21 12:13:08 +0900

    settings

---
+++ src/main/java/com/splguyjr/jenkins_test/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.splguyjr.jenkins_test.service;
+
+import com.splguyjr.jenkins_test.dto.CalculatorDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public int plusTwoNumbers(CalculatorDTO calculatorDTO) {
+        return calculatorDTO.getNum1() + calculatorDTO.getNum2();
+    }
+}
