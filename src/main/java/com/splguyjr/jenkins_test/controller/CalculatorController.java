c95f4a99d52d88ff16588ab19cdc40afc9b26c67
splguyjr <splguyjr@naver.com>
2025-05-22 09:15:22 +0900

    test code test

--- src/main/java/com/splguyjr/jenkins_test/controller/CalculatorController.java
+++ src/main/java/com/splguyjr/jenkins_test/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "jenkins pipeline test";
+        return "jenkins pipeline test 1";
     }
 
     @GetMapping("/plus")
